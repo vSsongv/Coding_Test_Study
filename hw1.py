f7d8dfefbdc048810d9b7960617b4fff4c707274
ssong <ssong@ssongui-MacBook-Pro.local>
2021-06-02 15:11:07 +0900

    Initial commit

---
+++ hw1.py
@@ -0,0 +1,11 @@
+# 입력값은 int(옷의 개수)
+# 한 박스에는 3 or 5개의 옷만 담을 수 있음
+# 박스의 개수를 최소화해서 보내야 함. 박스 개수를 return
+# 배송못하면 -1 return
+
+def main(c):
+    print("Main Function")
+
+if __name__ == "__main__":
+	c = 7
+	b = main(c)
