02e678789f01f17f02eccf239c51aff3132632ed
ssong <songdh21@naver.com>
2021-06-18 10:29:08 +0900

    pass first testcase

--- hotel_room.py
+++ hotel_room.py
@@ -8,20 +8,22 @@ def solution(k, room_number):
     answer = []
 
     rooms = {}
-    for i in range(k):  #k개의 방 넘버와, 선택되었는지 값을 가지고 있는 dict 생성
+    for i in range(1,k+1,1):  #k개의 방 num과, 선택되었는지 값을 가지고 있는 dict 생성
         rooms[i] = 0
 
-    for i, rn in enumerate(room_number):
-        if rooms[rn] == 0: #빈 방이면 value를 1로 바꾸고, 해당 room_number를 answer에 append.
-            rooms[rn] = 1
-            answer.append(rn)
+    for i,val in enumerate(room_number):
+        if rooms[val] == 0: #원하는 방이 빈 방이면
+            rooms[val] = 1 #value를 1로 바꾸고,
+            answer.append(val) #해당 room_number를 answer에 append.
         else:
-            for i, checked in enumerate(rooms):
-                if rooms[i] != 1:
-
-
-
+            for j in range(1, len(rooms), 1): #원하는 방이 빈 방이 아니면 rooms를 돌면서
+                if j > val: #원하는 방 num보다 크면서,
+                    if rooms[j] != 1:  #비어있는 가장 작은 방을 찾고
+                        rooms[j] = 1 #값 1로 바꾸고
+                        answer.append(j) #해당 방 넘버 append.
+                        break;
 
     return answer
 
-solution(k, room_number)
+rN = solution(k, room_number)
+print(rN)
