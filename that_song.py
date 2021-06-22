018e9b0acfdec6419db5f7a2cff83a2d73e3e396
송진영 <jy.song@crowdworks.kr>
2021-06-23 02:22:54 +0900

    check

---
+++ that_song.py
@@ -0,0 +1,7 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17683
+
+# m	musicinfos	answer
+# "ABCDEFG"	["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]	"HELLO"
+# "CC#BCC#BCC#BCC#B"	["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]	"FOO"
+# "ABC"	["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]	"WORLD"
+
