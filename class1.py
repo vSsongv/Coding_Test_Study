866e5e323346f00270072ccf6e2c86aabd38adc2
vSsongv <songdh21@naver.com>
2021-06-24 02:56:54 +0900

    second_class_1_ing

--- class1.py
+++ class1.py
@@ -33,7 +33,37 @@
 #         print(' ', end='')
 #     print("*"*i)
 
-# https://www.acmicpc.net/problem/2475
-# a,b,c,d,e = (map(int, input().split()))
-# verNum = (a*a + b*b + c*c + d*d + e*e)%10
-# print(verNum)
+# https://www.acmicpc.net/submit/2562
+# numbers = []
+# for _ in range(9):
+#     number = int(input())
+#     numbers.append(number)
+#
+# maxNum = max(numbers)
+# print(maxNum)
+# print(numbers.index(maxNum)+1)
+
+# https://www.acmicpc.net/problem/2577
+# a = int(input())
+# b = int(input())
+# c = int(input())
+#
+# result = list(str(a * b * c))
+# for i in range(10):
+#     print(result.count(str(i)))
+
+# https://www.acmicpc.net/problem/2675
+# tn = int(input())
+#
+# codes = []
+#
+# for n in range(tn):
+#     repeatNum, string = input().split()
+#     sentence = []
+#     for i in range(len(string)):
+#         sentence.append(string[i]*int(repeatNum))
+#
+#     code = "".join(sentence)
+#     codes.append(code)
+#
+#     print(codes[n])
