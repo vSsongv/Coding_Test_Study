72b9b83a627aa5973c084dc70bd99a0f384fa8b3
vSsongv <songdh21@naver.com>
2021-06-30 23:25:28 +0900

    need to CS study :(

--- class2_bronze.py
+++ class2_bronze.py
@@ -1,12 +1,32 @@
 # https://www.acmicpc.net/problem/4153 [직각삼각형]
+# import sys
+#
+# while True:
+#     triangles = list(map(int, sys.stdin.readline().split())) #리스트에 넣은 후
+#     triangles.sort() #소팅한다. sort()는 본체의 list를 정렬해서 반환하고, sorted는 정렬한 새로운 list를 반환한다.
+#     if triangles[0] == 0:
+#         break
+#     elif ((triangles[0]**2) + (triangles[1]**2)) == (triangles[2]**2): #소팅하면 제일 큰게 마지막 idx를 가진다.
+#         print("right")
+#     else:
+#         print("wrong")
+
+# https://www.acmicpc.net/problem/1920 [수 찾기]
+# 리스트의 in연산자를 통한 포함 여부의 시간 복잡도는 O(N)
+# 이분 탐색의 시간 복잡도는 O(logN)
+# Set과 Dictionary의 in연산을 통한 포함 여부 확인의 시간 복잡도는 O(1)
+# 해당 집합에 포함되는지 여부만 확인하면 되므로 Set 자료형을 사용해도 충분
 import sys
 
-while True:
-    triangles = list(map(int, sys.stdin.readline().split())) #리스트에 넣은 후
-    triangles.sort() #소팅한다. sort()는 본체의 list를 정렬해서 반환하고, sorted는 정렬한 새로운 list를 반환한다.
-    if triangles[0] == 0:
-        break
-    elif ((triangles[0]**2) + (triangles[1]**2)) == (triangles[2]**2): #소팅하면 제일 큰게 마지막 idx를 가진다.
-        print("right")
+nothing = int(input())
+base_list = set(map(int, sys.stdin.readline().split()))
+num = int(input())
+target_list = list(map(int, sys.stdin.readline().split()))
+
+for i in target_list:
+    if i in base_list:
+        print('1')
     else:
-        print("wrong")
+        print('0')
+
+
