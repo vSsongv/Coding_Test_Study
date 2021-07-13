df737c2c44675e1c50cb9ce99bfe9545a814e79f
vSsongv <songdh21@naver.com>
2021-07-13 23:44:06 +0900

    add explain

--- stepping_stone.js
+++ stepping_stone.js
@@ -1,4 +1,4 @@
-// https://programmers.co.kr/learn/courses/30/lessons/43236
+// https://programmers.co.kr/learn/courses/30/lessons/43236 [징검다리]
 function solution(distance, rocks, n) {
     let answer = 0;
     rocks=[0,...rocks.sort((a,b)=>a-b),distance];
