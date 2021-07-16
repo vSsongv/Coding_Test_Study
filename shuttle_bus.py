d84934376611350d7b533a431ce9c13b82299658
vSsongv <songdh21@naver.com>
2021-07-17 03:47:05 +0900

    draft

--- shuttle_bus.py
+++ shuttle_bus.py
@@ -6,44 +6,41 @@
 import copy
 
 def solution(n, t, m, timetable):
+    answer = ''
+    def H_to_M(time): #시간을 분으로 바꾸는 함수
+        H,M = map(int, time.split(":"))
+        return H * 60 + M
 
-    # timetable = [int(time[:2]) * 60 + int(time[3:5]) for time in timetable]
-    # timetable.sort()
-    # last_time = (60 * 9) + (n - 1) * t
-    # for i in range(n):
-    #     if len(timetable) < m:
-    #         return '%02d:%02d' % (last_time // 60, last_time % 60)
-    #     if i == n - 1:
-    #         if timetable[0] <= last_time:
-    #             last_time = timetable[m - 1] - 1
-    #         return '%02d:%02d' % (last_time // 60, last_time % 60)
-    #     for j in range(m - 1, -1, -1):
-    #         bus_arrive = (60 * 9) + i * t
-    #         if timetable[j] <= bus_arrive:
-    #             del timetable[j]
+    def M_to_H(minute):#분을 시간으로 바꾸는 함수
+        H,M = divmod(minute, 60)
+        if H < 10:
+            H = "0" + str(H)
+        if M < 10:
+            M = "0" + str(M)
+        return str(H) + ":" + str(M)
 
-    start_time = 540
-    timetable = [int(time[:2]) * 60 + int(time[3:5]) for time in timetable]
-    timetable.sort()
+    start_time = H_to_M("09:00") #분으로 바꾸고
+    timetable.sort()  # 먼저 timetable내부 값들 sort
     clone_tt = copy.deepcopy(timetable)
 
-    for i in range(n):
+    for _ in range(n): #버스는 n번 오기때문에.
+        start_time += t #지금 버스 출발시간에 t만큼 더해줌
         totalCrewNum = 0
+        index = 0 #sort했으니까 제일 앞에 애만 보면 됨.
         while totalCrewNum < m and len(clone_tt) > 0:  # m이랑 같아질 때까지
-            if clone_tt[0] == 1440: #sort했으니까 제일 앞에 애만 보면 됨.
-                clone_tt[0] = 1439
-            if clone_tt[0] <= start_time: #출발시간보다 일찍 왔으면
+            if H_to_M(clone_tt[index]) <= start_time: #출발시간보다 일찍 왔으면
                 totalCrewNum += 1 #타세요
-                del clone_tt[0]
+                clone_tt.remove(clone_tt[index])
+                print(clone_tt)
             else: #다음 크루가 현재 버스의 출발시간보다 늦게 왔다면
                 break;
-        start_time += t  # 지금 버스 출발시간에 t만큼 더해줌
 
     if totalCrewNum < m:
-        answer = '%02d:%02d'%((start_time - t)/60,(start_time - t)%60)
+        print(totalCrewNum)
+        answer = M_to_H(start_time-t)
     else:
-        last_time = timetable[-1] - 1
-        answer = '%02d:%02d'%(last_time/60,last_time%60)
+        answer = M_to_H(H_to_M(timetable[-1]) - 1)
     print(answer)
+    return answer
 
-solution(1,	1,	5,	["08:00", "08:01", "08:02", "08:03"])
+solution(2,	10,	2,	["09:10", "09:09", "08:00"])
