ee3e1d5bb7f4a3481c0ddd767151626ebc13c899
vSsongv <songdh21@naver.com>
2021-08-10 23:02:41 +0900

    done

--- BOJ/class2_bronze.js
+++ BOJ/class2_bronze.js
@@ -1,4 +1,4 @@
-// https://www.acmicpc.net/problem/2798 [블랙잭] - 브루트 포스(brute force)
+// https://www.acmicpc.net/problem/2798 [블랙잭] - 브루트 포스(brute force) 알고리즘 : 무식한 완전탐색 알고리즘!
 // https://tesseractjh.tistory.com/4(참고)
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(/\s/).map(item => parseInt(item));
 
@@ -7,7 +7,7 @@ let M = parseInt(input[1]);
 let sum = 0;
 let cards = input.slice(2); //2번째 줄부터 입력받은 숫자들을 배열 형태로 저장.
 
-for(let i = 0; i < N-2; i++){
+for(let i = 0; i < N-2; i++){ 
     for(let j = i+1; j < N-1; j++){
         for(let k = j+1; k < N; k++){
             if(cards[i]+cards[j]+cards[k] <= M && cards[i]+cards[j]+cards[k] > sum){
