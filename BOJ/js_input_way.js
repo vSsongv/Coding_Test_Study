18c288940644a7042eb7df217484cb6a7748b3c3
vSsongv <songdh21@naver.com>
2021-08-16 23:55:46 +0900

    input way

--- BOJ/js_input_way.js
+++ BOJ/js_input_way.js
@@ -1,5 +1,18 @@
-//2줄, 3줄일 경우
+//1줄 입력
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+rl.on("line", function(line) {
+  console.log("hello !", line);
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
+
+//2줄, 3줄일 경우
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,3 +28,13 @@ rl.on('line', function (line) {
     console.log(input);
     process.exit();
 });
+
+//백준 입력 예제 받아서 실행할 수 있음
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split("\n");
+
+const data=Number(input);
+
+for (let i=1;i<=data;i++){
+ console.lot(i);
+ }
