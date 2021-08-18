2bf4341d5e15e623f98898abcd2e88f706e5c06f
vSsongv <songdh21@naver.com>
2021-08-18 23:59:37 +0900

    edit

--- Programmers/shuttle_bus.py
+++ Programmers/shuttle_bus.py
@@ -3,8 +3,6 @@
 #09:00부터 n회 t분 간격으로, m명이 탈 수 있다.
 # n  t	m	timetable	이
 # 1  1	5	["08:00", "08:01", "08:02", "08:03"]인 경우, "09:00"부터 운행하는데 4명이 서 있기 때문에 콘은 09:00에 와도 탈 수 있다.
-import copy
-
 def solution(n, t, m, timetable):
     answer = ''
     def H_to_M(time): #시간을 분으로 바꾸는 함수
