2ec012201b7961ab68fdd7ad60a1390f8aa71d31
vSsongv <songdh21@naver.com>
2021-08-24 00:49:08 +0900

    -ing

--- Programmers/lock&key.js
+++ Programmers/lock&key.js
@@ -1,6 +1,6 @@
 // https://programmers.co.kr/learn/courses/30/lessons/60059 [자물쇠와 키]
 
-function rotate(key) { //90도 rotate function
+function rotate(key) { //90도 rotate func
     let len = key.length;
     let new_key = Array.from(Array(len), () => Array(len).fill(0));
     for(let i = 0; i < len; i++) {
@@ -16,12 +16,18 @@ function solution(key, lock) {
     
     const len = lock.length;
     const bigRock = Array.from(Array(len + (key.length-1) * 2), () => Array(len + (key.length-1) * 2).fill(0));
-    
-    for (let i = len-1; i < (len-1) * 2; i++) {
-        for (let j = len-1; j < (len-1) * 2; j++) {
-            bigRock[i][j] = lock[i - len][j - len];
-            console.log(i,j);
+     
+    let start_coor = len-1;
+    for (let i = start_coor; i <= (start_coor) * 2; i++) {
+        for (let j = start_coor; j <= (start_coor) * 2; j++) {
+            bigRock[i][j] = lock[i - start_coor][j - start_coor];  //insert lock in bigRock.
         }
     }
-    return answer;   
+    
+    for(let x = 0; x < bigRock.length; x++) {
+        for(let y = 0; y < )
+    }
+    
+    return answer;
+    
 }
