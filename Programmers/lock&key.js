a5228f76defcaa3a17460dd0314bb258a303c5c6
vSsongv <songdh21@naver.com>
2021-08-27 00:57:34 +0900

    -ing

--- Programmers/lock&key.js
+++ Programmers/lock&key.js
@@ -1,6 +1,6 @@
 // https://programmers.co.kr/learn/courses/30/lessons/60059 [자물쇠와 키]
 
-function rotate(key) { //90도 rotate func
+function rotate(key) { //rotate function
     let len = key.length;
     let new_key = Array.from(Array(len), () => Array(len).fill(0));
     for(let i = 0; i < len; i++) {
@@ -15,18 +15,12 @@ function solution(key, lock) {
     var answer = true;
     
     const len = lock.length;
-    const bigRock = Array.from(Array(len + (key.length-1) * 2), () => Array(len + (key.length-1) * 2).fill(0));
+    const bigRock = Array.from(Array(len + (key.length-1) * 2), () => Array(len + (key.length-1) * 2).fill(0)); //make array that has big size of lock.
      
     let start_coor = len-1;
     for (let i = start_coor; i <= (start_coor) * 2; i++) {
         for (let j = start_coor; j <= (start_coor) * 2; j++) {
-            bigRock[i][j] = lock[i - start_coor][j - start_coor];  //insert lock in bigRock.
-        }
-    }
-    
-    for(let x = 0; x < bigRock.length - 2; x++) {
-        for(let y = 0; y < bigRock.length - 2; y++) {
-            bigRock[x][y] = key[x][y]
+            bigRock[i][j] = lock[i - start_coor][j - start_coor]; //insert lock in the middle of big array
         }
     }
     
