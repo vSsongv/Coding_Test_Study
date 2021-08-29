fae67db968e16ea10572f65054a5cd32a4883b95
vSsongv <songdh21@naver.com>
2021-08-30 01:07:50 +0900

    -ing

--- Programmers/lock&key.js
+++ Programmers/lock&key.js
@@ -2,36 +2,45 @@
 
 function rotate(key) { //rotate function
     let len = key.length;
-    let new_key = Array.from(Array(len), () => Array(len).fill(0));
+    let new_array = Array.from(Array(len), () => Array(len).fill(0));
     for(let i = 0; i < len; i++) {
         for(let j = 0; j < len; j++) {
-            new_key[len-i-1][j] = key[j][i];
+            new_array[i][j] = key[len - 1 - j][i];
         }
     }
-    return new_key;
+    return new_array;
 }
 
-function isLock(tmp_Rock, rockLen, keyLen) {
+function isOpenk(tmp_Rock, rockLen, keyLen) {
     let start_coor = keyLen-1;
     for(let i = start_coor; i <= rockLen-keyLen; i++) {
         for(let j = start_coor; j <= rockLen-keyLen; j++) {
-            if(tmp_Rock[i][j] != 1) return false;
+            if(tmp_Rock[i][j] != 1) return false; 
         }
     }
     return true;
 }
 
 function solution(key, lock) {
-    
+        
     const rockLen = lock.length;
     const keyLen = key.length;
+    let sum = 0;
+    
+    for(let z = 0; z < rockLen; z++) {
+        for(let w = 0; w < rockLen; w++) {
+            sum += lock[z][w];
+        }
+    }
+    
+    if(sum == rockLen * rockLen) return true;
     
     const bigRock = Array.from(Array(rockLen + (keyLen-1) * 2), () => Array(rockLen + (keyLen-1) * 2).fill(0)); //make array that has big size of lock.
      
                 
     let start_coor = keyLen-1;
-    for(let i = start_coor; i <= rockLen-keyLen; i++) {
-        for(let j = start_coor; j <= rockLen-keyLen; j++) {
+    for(let i = start_coor; i <= bigRock.length-keyLen; i++) {
+        for(let j = start_coor; j <= bigRock.length-keyLen; j++) {
             bigRock[i][j] = lock[i - start_coor][j - start_coor]; //insert lock in the middle of big array
         }
     }
@@ -47,10 +56,10 @@ function solution(key, lock) {
                 
                 for(let k = 0; k < keyLen; k++) { 
                     for(let l = 0; l < keyLen; l++) {
-                        tmp_Rock[x+l][y+k] += key[k][l];                        
+                        tmp_Rock[x+k][y+l] += key[k][l];
                     }
                 }
-                if(isLock(tmp_Rock, rockLen, keyLen)) return true;
+                if(isOpen(tmp_Rock, bigRock.length, keyLen)) return true;
             }
         }
         c++;
