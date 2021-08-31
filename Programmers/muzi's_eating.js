4c0e4842c3a972937d982d0b83b145188aa5d38b
vSsongv <songdh21@naver.com>
2021-09-01 04:19:31 +0900

    timeout for only1

--- Programmers/muzi's_eating.js
+++ Programmers/muzi's_eating.js
@@ -1,38 +1,34 @@
-
-// https://programmers.co.kr/learn/courses/30/lessons/42891?language=javascript# [무지의 먹방 라이브]
-
 function solution(food_times, k) {
 
     let foodNum = food_times.length;
     let time = 0;
     let doneFood = 0;
     let i = 0;
-    let nextFood = 0;
     
     while(time < k) { //elecrticity is ok yet
         if(food_times[i] != 0) {
             food_times[i] -= 1; //eat 1 sec
             i++;  
             time++;
-            console.log('i',i);
-            console.log('t',time);
         }
         else {
             i++;
-            console.log('ei',i);
         }
         if(i > foodNum-1) i = 0; //elecrticity is ok yet but rotated one food arr
     }
     
-    while(food_times[i] == 0 && i > foodNum-1) {
-        i++;
-        if(i > foodNum-1) i = 0;
-        doneFood++;  
-        console.log(i);
+    if(food_times[i] == 0) {
+        for(let j = 0; j < foodNum; j++) { //loop only for size of food_times
+            if(food_times[i] == 0) {
+                doneFood++;
+                i++;
+                if(i > foodNum-1) i = 0;
+            }
+            else break;
+        }
     }
-    console.log(i);
+
     if(doneFood == foodNum) return -1;
-    if(i > foodNum-1) i = 0;
     
-    return (i+1 < foodNum) ? i+1 : i;
+    return i+1;
 }
