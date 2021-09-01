941b49d2adc7ff6dcb1010120a990241a6289eda
vSsongv <songdh21@naver.com>
2021-09-02 01:11:15 +0900

    efficiency check!

--- Programmers/muzi's_eating.js
+++ Programmers/muzi's_eating.js
@@ -1,3 +1,5 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42891 [무지의 먹방]
+
 function solution(food_times, k) {
 
     let foodNum = food_times.length;
@@ -8,27 +10,26 @@ function solution(food_times, k) {
     while(time < k) { //elecrticity is ok yet
         if(food_times[i] != 0) {
             food_times[i] -= 1; //eat 1 sec
-            i++;  
+            if(food_times[i] == 0) {
+                doneFood++;
+                if(doneFood == foodNum) return -1;
+            }
             time++;
         }
-        else {
-            i++;
-        }
+        i+=1;
         if(i > foodNum-1) i = 0; //elecrticity is ok yet but rotated one food arr
     }
     
-    if(food_times[i] == 0) {
-        for(let j = 0; j < foodNum; j++) { //loop only for size of food_times
-            if(food_times[i] == 0) {
-                doneFood++;
-                i++;
-                if(i > foodNum-1) i = 0; //update i
-            }
-            else break; //if not 0 val, break;
-        }
-    }
-
     if(doneFood == foodNum) return -1;
     
-    return i+1; //i is index, so add 1
+    if(food_times[i] == 0) {
+        for(let j = 0; j < foodNum; j++) { //loop when 
+            if(food_times[i] == 0) {
+                i += 1;
+                if(i > foodNum-1) i = 0;
+            }
+            else break;
+        }
+    }    
+    return i+1;
 }
