b78f0fbed90525a864cd2c8a9ca6456b8c536469
vSsongv <songdh21@naver.com>
2021-09-02 18:01:54 +0900

    making dict done

--- Programmers/search_rank.py
+++ Programmers/search_rank.py
@@ -9,26 +9,31 @@ query = ["java and backend and junior and pizza 100", "python and frontend and s
          "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100",
          "- and - and - and - 150"]
 
-
-def makeDict(info):
-    option_dict = {}
-    for p in info: #info를 돌면서
-        tmp = p.split() #자르고
-        character = tmp[0:4] #점수 앞부분
-        score = int(tmp[-1]) #점수
-
-        for i in range(4): #16개의 경우 생성
-            print(i)
-            for j in range(4):
-                key = character[i][j]
-
-
-        # print(character)
-
+from itertools import combinations
 
 def solution(info, query):
+
+    def makeDict(infos, dict): # make 16(4*4) key
+        for p in infos:  # for all info values
+            tmp = p.split()  # seperate
+            info= tmp[0:4]  # will be keys
+            score = int(tmp[-1])  # score = value
+
+            for i in range(5):
+                for i in combinations(info, i):
+                    key = ''.join(i)
+                    if key in dict:
+                            dict[key].append(score) #value is score
+                    else:
+                        dict[key] = []
+                        dict[key].append(score) #value is score
+        return dict
+
     answer = []
-    makeDict(info)
+    dict = {}
+    option_dict = makeDict(info, dict)
+    print(option_dict)
+
 
     return answer
 
