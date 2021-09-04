85d2843bd93a6f62635b5c98038e062567b14568
vSsongv <songdh21@naver.com>
2021-09-05 03:06:40 +0900

    -ing

--- Programmers/muzi's_eating.js
+++ Programmers/muzi's_eating.js
@@ -1,33 +1,44 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42891 [무지의 먹방]
 
 function solution(food_times, k) {
-
     let foodNum = food_times.length;
-    let time = 0;
-    let doneFood = 0;
-    let i = 0;
-    
-    while(time < k) { //elecrticity is ok yet
-        if(food_times[i] != 0) {
-            food_times[i] -= 1; //eat 1 sec
-            if(food_times[i] == 0) {
-                doneFood++;
-                if(doneFood == foodNum) return -1;
-            }
-            time++;
-        }
-        i+=1;
-        if(i > foodNum-1) i = 0; //elecrticity is ok yet but rotated one food arr
+    var answer = 0;
+    let times_dict = {};
+    let times = [];
+    let priority = [];
+    for(let i = 0; i < foodNum; i++) {
+        times_dict[i] = food_times[i];
     }
     
-    if(food_times[i] == 0) {
-        for(let j = 0; j < foodNum; j++) { //loop when 
-            if(food_times[i] == 0) {
-                i += 1;
-                if(i > foodNum-1) i = 0;
-            }
-            else break;
+    let prio_times = [];
+    for(let time in times_dict) {
+        prio_times.push([time, times_dict[time]]);
+    }
+
+    prio_times.sort(function(a, b) {
+        return a[1] - b[1];
+    });
+    
+    for(let i = 0; i < foodNum; i++) {
+        times[i] = prio_times[i][1];
+        priority[i] = Number(prio_times[i][0]);
+    }
+    
+    let sumOfTime = 0
+    let differ = 0;
+    while(times) {
+        times[0] = times[0] - differ //update munimun val
+        sumOfTime = times[0] * foodNum; //subtract minimum time from all time val
+        if(k < sumOfTime) { //shutdowned before eat all food
+            priority.sort(); //sort priority
+            return priority[k % foodNum]+1; //return index+1
         }
-    }    
-    return i+1;
+        k = k - sumOfTime //update k
+        differ += times[0]; //update difference
+        times.shift(); //pop
+        priority.shift(); //pop
+        if(times.length == 0) return -1; //ate all food before shut down.
+        foodNum -= 1;
+    }
+    return -1; //ate all food after shut down
 }
