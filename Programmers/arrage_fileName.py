f3bac2dab77c413bce3a262aa195e94a8de958e2
vSsongv <songdh21@naver.com>
2021-09-09 04:02:36 +0900

    done!

--- Programmers/arrage_fileName.py
+++ Programmers/arrage_fileName.py
@@ -3,7 +3,7 @@
 import re
 
 def solution(files):
-    splited = [re.split("([0-9]+)", file) for file in files]
-    sortedFiles = sorted(splited, key=lambda point: (point[0].lower(), int(point[1])))
+    splited = [re.split("([0-9]+)", file) for file in files] #split by number
+    sortedFiles = sorted(splited, key=lambda point: (point[0].lower(), int(point[1]))) #sort by two keys.(name, num)
 
-    return [''.join(fileName) for fileName in sortedFiles]
+    return [''.join(fileName) for fileName in sortedFiles] #join all values
