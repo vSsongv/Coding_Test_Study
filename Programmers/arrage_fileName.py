a008de4db6a4871cb70330264e787f1c1ff706c2
vSsongv <songdh21@naver.com>
2021-09-08 23:50:46 +0900

    -ing

--- Programmers/arrage_fileName.py
+++ Programmers/arrage_fileName.py
@@ -1,6 +1,10 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17686 [파일명 정렬]
 
+import re
+
 def solution(files):
     answer = []
-    files.split('.') #ext
+    splited = [re.split("([0-9]+)", file) for file in files]
+    sortedFiles = sorted(splited, key = lambda point: (point[0], point[1]))
+    print(sortedFiles)
     return answer
