870eed070c3fd552b7558bffacb51a0252631b92
vSsongv <songdh21@naver.com>
2021-09-09 21:34:26 +0900

    del

--- Programmers/that_song.py
+++
@@ -1,8 +0,0 @@
-# https://programmers.co.kr/learn/courses/30/lessons/17683 [방금그곡]
-# need to solve!
-
-m	musicinfos	answer
-"ABCDEFG"	["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]	"HELLO"
-"CC#BCC#BCC#BCC#B"	["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]	"FOO"
-"ABC"	["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]	"WORLD"
-
