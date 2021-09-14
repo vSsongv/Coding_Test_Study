e384a564882c0800ec9a3a96c4b5dc87ec405ece
vSsongv <songdh21@naver.com>
2021-09-15 03:10:01 +0900

    test_case_done_only

--- Programmers/arrange_boxer.py
+++ Programmers/arrange_boxer.py
@@ -1,3 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/85002 [복서 정렬하기]
+
 def solution(weights, head2head):
     playerNum = len(weights)-1
     answer = []
@@ -8,8 +10,16 @@ def solution(weights, head2head):
         
     for p in range(len(head2head)):
         player[p+1]['winRate'] = head2head[p].count('W') / playerNum
-    
-    player = sorted(player.items(), key = lambda k: (k[1]['winRate'], k[1]['weight']), reverse=True)
-    print(player)
         
+    for h in range(len(head2head)):
+        for r in range(len(head2head[h])):
+            if head2head[h][r] == 'W':
+                if player[r+1]['weight'] > player[h+1]['weight']:
+                    player[h+1]['winOver'] += 1
+    
+    sortedPlayer = sorted(player.items(), key = lambda k: (k[1]['winRate'], k[1]['winOver'], k[1]['weight']), reverse=True)
+    
+    for boxer in sortedPlayer:
+        answer.append(boxer[0])
+
     return answer
