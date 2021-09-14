22163189cf61fadfc23c319c33b89b82080f944a
vSsongv <songdh21@naver.com>
2021-09-14 20:18:55 +0900

    done!

--- Programmers/column&beam.py
+++ Programmers/column&beam.py
@@ -1,27 +1,31 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60061?language=python3 [기둥과 보 설치]
 
-def solution(n, build_frame):
+def check(answer):
+    for x, y, what in answer:
+        if what == 0: #column
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer: #column rule(on floor, on other col, on part of beam)
+                continue
+            else: return False
+        else: #beam
+            if [x,y-1,0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer): #beam rule(on other col, connect with other beam)
+                continue
+            else: return False
+    return True
+
+def solution(n, build_frame):    
     answer = []
-    built_dict = {}
-
-    for step in build_frame:
-        if (step[3] == 1):  # when build
-            if ((step[0],step[1]) in built_dict):  # built before
-                if (step[2] == 0):  # is colum
-                    if (step[1] != 0):  # if location is not floor
-                        if (built_dict[(step[0],step[1])].includes(1)):  # beam has to be therd
-                            built_dict[(step[0],step[1])].append(0)  # if there is beam, can bulid
-                        else: #if not beam there, fail
-                            built_dict[(step[0],step[1])] = []
-                    else:  # if floor
-                        continue
-                else:  # is beam
-                    built_dict[(step[0],step[1])] = step[2]
-            else:  # first bulid
-                built_dict[(step[0],step[1])] = [step[3]]  # appen new col
-
-        else:  # when delete
-            built_dict[(step[0],step[1])] = step[2]
-        print(built_dict)
+    for frame in build_frame:
+        x,y,what,how = frame
+        
+        if how == 1: #build
+            answer.append([x,y,what])
+            if check(answer) is False:
+                answer.remove([x,y,what])
+        else: #delete
+            answer.remove([x,y,what])
+            if check(answer) is False:
+                answer.append([x,y,what])
+        
+        answer.sort()
 
     return answer
