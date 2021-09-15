bbd172df1073fc862b1519574a901b00192a6008
vSsongv <songdh21@naver.com>
2021-09-16 01:21:38 +0900

    move_folder

--- Programmers/2020_kakao_Recr/lock&key.js
+++ Programmers/2020_kakao_Recr/lock&key.js
@@ -1,4 +1,4 @@
-// https://programmers.co.kr/learn/courses/30/lessons/60059 [자물쇠와 키]
+// https://programmers.co.kr/learn/courses/30/lessons/60059 [자물쇠와 ]
 
 function rotate(key) { //rotate function
     let len = key.length;
