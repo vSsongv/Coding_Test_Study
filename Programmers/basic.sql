7ad8232f03d32b352d068e0b59b141cef035c28e
vSsongv <songdh21@naver.com>
2021-09-23 00:44:15 +0900

    studying spl!

---
+++ Programmers/basic.sql
@@ -0,0 +1,4 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59034 [모든 레코드 조회하기]
+SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID --order by val val로 오름차순 정렬
+-- https://programmers.co.kr/learn/courses/30/lessons/59035 [역순 정렬하기]
+SELECT NAME,DATETIME FROM ANIMAL_INS ORDER BY ANIMAL_ID DESC --order by val DESC 내림차순 정렬
