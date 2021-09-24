0559e77e53ecadc4b30a137e8ca71bb2ba8c9535
vSsongv <songdh21@naver.com>
2021-09-24 22:50:33 +0900

    done!

---
+++ Programmers/failed_runner.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42576?language=python3 [완주하지 못한 선수]
+
+def solution(participant, completion):
+    answer = ''
+    participant.sort()
+    completion.sort()
+    completion.append("1")
+    
+    for p in range(len(participant)):
+        if participant[p] != completion[p]:
+            return participant[p]
