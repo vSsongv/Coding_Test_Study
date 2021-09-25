76174d8025a7c5f23b3b8d4cc8e4aee1fc852355
vSsongv <songdh21@naver.com>
2021-09-26 00:26:02 +0900

    delete console.log lines

--- Programmers/2020_kakao_Recr/transfer_bracket.js
+++ Programmers/2020_kakao_Recr/transfer_bracket.js
@@ -15,14 +15,12 @@ function solution(p) {
 		if (right > left) check = true; // )가 (보다 많으면 틀린 모양임
         
 		if (right == left) { //균형 잡힌 문자열임
-            console.log("ddd",p.substring(0,i+1));
             if(check == true) { //틀린 모양이면
                 answer += "("; //빈 문자열에 첫 번째 문자로 '('를 붙임
                 answer += solution(p.substring(i+1)); //뒷부분 재귀 결과 문자열 붙임
                 answer += ")"; //')'를 다시 붙임
                 if(i!= 1) {
                     let s = p.substring(1,i);
-                    console.log("s",s);
                     answer += s.replace(/\(|\)/g, a => a === "(" ? ")" : "(" ); //u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙임
                 }
                 return answer;
