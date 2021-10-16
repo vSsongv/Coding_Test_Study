d7154740c11a0823b836b3599be7529614a4d174
vSsongv <songdh21@naver.com>
2021-10-17 00:28:12 +0900

    done! 입력값 잘 보기

--- BOJ/hide&seek.js
+++ BOJ/hide&seek.js
@@ -3,24 +3,25 @@
 const input = require('fs').readFileSync('input.txt').toString().trim().split(' ');
 // const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
 
+let checked = new Array(100001).fill(0);
+let curr = Number(input[0]);
+let loca = Number(input[1]);
 let queue = [];
-let checked = new Array(10001).fill(0);
-let curr = input[0];
-let loca = input[1];
-let len = queue.length;
 let L = 0;
 
-function BFS(curr, loca) {
+function BFS() {
     queue.push(curr); //queue에 현재 위치 push
     checked[curr] = 1; //확인한 경로이므로 checked배열에 check
-    while(len) {
-        for(let i = 0; i < len; i++) {
+    while (queue.length) {
+        let len = queue.length;
+        for (let i = 0; i < len; i++) {
             let v = queue.shift(); // 처음 값을 빼주고,
-            for(let nv of [curr - 1, curr + 1, curr * 2]) {
-                if(nv === loca) { //다음으로 이동할 위치가 동생이 있는 위치라면 현재  level(이동한 초) 리턴
-                    return L;
+            for (let nv of [v - 1, v + 1, v * 2]) {
+                if (nv === loca) { //다음으로 이동할 위치가 동생이 있는 위치라면 현재  level(이동한 초) 리턴
+                    return L + 1;
                 }
-                if(nv > 0 && nv <= 10000 && checked[nv] === 0) {//확인하지 않은 경로라면 탐색.
+                if (nv >= 0 && nv <= 100000 && checked[nv] === 0) {//확인하지 않은 경로라면 탐색.
+                    checked[nv] = 1;
                     queue.push(nv);
                 }
             }
@@ -29,5 +30,6 @@ function BFS(curr, loca) {
     }
 }
 
-if(curr === loca) return 0;
-BFS(curr, loca);
+if (curr === loca) console.log(0);
+else console.log(BFS());
+
