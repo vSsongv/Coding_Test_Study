dc17b916dccc8ce68d488b0d0cb1941bff7dcb2a
vSsongv <songdh21@naver.com>
2021-10-16 23:33:24 +0900

    done

--- BOJ/invite_villageCap.py
+++ BOJ/invite_villageCap.py
@@ -4,12 +4,8 @@ import sys
 num = int(input())
 time_list = list(map(int, sys.stdin.readline().split()))
 time_list.sort(reverse=True) # 내림차순으로 정렬해주기
-answer = 2
 
-for i in range(1, num):
-    if time_list[i-1] == time_list[i]: answer += 1
-    else: answer += (time_list[i-1] - time_list[i])
-    print(answer)
-answer += time_list[-1]
+for i in range(num):
+    time_list[i] = i + 1 + time_list[i] #나무를 심는 날 + 나무가 자라는 데 걸리는 시간이 다 자라는 시간임.
 
-print(answer)
+print(max(time_list)+1);
