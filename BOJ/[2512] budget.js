71b0328c7bef092afff1ae2d5c73107d4e687786
vSsongv <songdh21@naver.com>
2021-10-19 11:27:06 +0900

    need to study

--- BOJ/[2512] budget.js
+++ BOJ/[2512] budget.js
@@ -4,42 +4,24 @@
 let Path = "input.txt"
 //로컬 테스트시 주석
 // let Path = "/dev/stdin"
-const input = require('fs').readFileSync(Path).toString().trim().split('\n');
+const input = require("fs").readFileSync(Path).toString().trim().split("\n");
 let num = input[0];
-let budgets = [];
-for (let i = 0; i < num; i++) {
-    budgets.push(Number(input[1].split(' ')[i]));
-}
-budgets.sort((a, b) => a - b);
-
+let budgets = new Array(num);
+budgets = input[1].split(" ").map(Number);
 let setMoney = Number(input[2]);
-let sum = budgets.reduce((a, b) => a + b);
 
-if (sum <= setMoney) { //모두 배분 가능하면
-    console.log(Math.max(...budgets));
-}
-else {
-    function getSum(mid) {
-        let sum = 0;
-        budgets.forEach(a => {
-            sum += Math.min(mid, a)
-        })
-        console.log(sum);
-        return sum
+let max = Math.max(...budgets);
+let min = 0, propalBudget = 0;
+while (min <= max) {
+    let mid = parseInt((min + max) / 2);
+    let sum = 0;
+    budgets.forEach(a => {
+        sum += Math.min(mid, a)
+    });
+    if (sum > setMoney) max = mid - 1;
+    else {
+        min = mid + 1;
+        propalBudget = Math.max(propalBudget, mid);
     }
-
-    let max = budgets[num-1];
-    let min = 0, propalBudget = 0;
-    while (min <= max) {
-        let mid = parseInt((min + max) / 2);
-        let tmp = getSum(mid)
-        console.log(mid, tmp);
-        if (tmp > setMoney) {max = mid - 1; console.log("m", mid, "t", tmp, "max", max);}
-        else {
-            min = mid + 1;
-            console.log("m", min, "d", mid, "p",propalBudget);
-            if (propalBudget < min) propalBudget = mid;
-        }
-    }
-    console.log(propalBudget);
 }
+console.log(propalBudget);
