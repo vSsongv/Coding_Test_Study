7a83547b82f66e3bb84797377e1f3ef39aa5e0eb
vSsongv <songdh21@naver.com>
2021-11-06 01:57:23 +0900

    chang_to_js

---
+++ Programmers/2022_kakao_Recr/mailing_benList.js
@@ -0,0 +1,49 @@
+function solution(id_list, report, k) {
+    let answer = [];
+    let mail = {};
+    let ben_list = []; // k번이상 신고된 사람들.
+    let id_dict = {};
+    let reported_dict = {};  // 신고당한 대상들
+
+    for (let ID of id_list) {
+        id_dict[ID] = [];  // id가 신고한 대상들
+        reported_dict[ID] = 0;
+        mail[ID] = 0;
+    }
+
+    for (let re of report) {
+        let tmp = re.split(' ');
+        let reporter = tmp[0];
+        let reported = tmp[1];
+        if (!(id_dict[reporter].includes(reported))) id_dict[reporter].push(reported);
+    }
+
+    for (let [re, eds] of Object.entries(id_dict)) {
+        for (let ed of eds) {
+            reported_dict[ed] += 1;
+        }
+    }
+    console.log(reported_dict)
+    for (let [key, val] of Object.entries(reported_dict)) {
+        if (val >= k) {
+            ben_list.push(key);
+        }
+    }
+
+    for (let ben of ben_list) {
+        for (let [re, ed] of Object.entries(id_dict)) {
+            console.log(re, ed)
+            if (ed.includes(ben)) {
+                mail[re] += 1;
+            }
+        }
+    }
+
+    for (let [k, v] of Object.entries(mail)) {
+        answer.push(v);
+    }
+    return answer;
+}
+
+// console.log(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"], 2))
+console.log(solution(["con", "ryan", "ryan", "con"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3));
