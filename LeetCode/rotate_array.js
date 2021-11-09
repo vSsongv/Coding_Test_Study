d57bb2c8170d0f277f2c599b344dd8150ac5d932
vSsongv <songdh21@naver.com>
2021-11-09 10:53:23 +0900

    done

--- LeetCode/rotate_array.js
+++ LeetCode/rotate_array.js
@@ -1,9 +1,30 @@
 // https://leetcode.com/problems/rotate-array/ [189-Rotate Array]
 
 //time limited난 풀이
+// var rotate = function (nums, k) {
+//     for (let i = 0; i < k; i++) {
+//         nums.unshift(nums.pop());
+//     }
+//     return nums;
+// };
+
 var rotate = function (nums, k) {
-    for (let i = 0; i < k; i++) {
-        nums.unshift(nums.pop());
+    function reverse2(start, end) {
+        while (start < end) {
+            let tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+        return nums;
     }
+    k %= nums.length;
+    nums = nums.reverse();
+    nums = reverse2(0, k - 1);
+    nums = reverse2(k, nums.length - 1);
     return nums;
 };
+
+console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
+console.log(rotate([-1], 2));
