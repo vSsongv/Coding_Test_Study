56b9e813ece5be32a1572f3677406ff7b80b4cdd
vSsongv <songdh21@naver.com>
2021-12-12 02:34:07 +0900

    ♻️ refactoring

--- Programmers/2021_kakao_Recr/new_id.js
+++ Programmers/2021_kakao_Recr/new_id.js
@@ -6,7 +6,6 @@
 function solution(new_id) {
     var answer = '';
     answer = MakeLong(MakeShort(Create(DeleteOneDot(ChangeDots(DeleteWrongCont(UtoL(new_id)))))));
-    console.log(answer);
     return answer;
 }
 
@@ -15,34 +14,31 @@ function UtoL(new_id) {
 }
 
 function DeleteWrongCont(new_id) { //step 2
-    return new_id.replace(/[^0-9a-z-_.]/g, "");
+    return new_id.replace(/[^a-z0-9-_.]/g, "");
 }
 
 function ChangeDots(new_id) { //step 3
-    return new_id.replace(/[.]+/g,".");
+    return new_id.replace(/[.]+/g, ".");
 }
 
 function DeleteOneDot(new_id) { //step 4
-    return new_id.replace(/^[.]|[.]$/g,"");
+    return new_id.replace(/^[.]|[.]$/g, "");
 }
 
 function Create(new_id) { //step 5
-    if(new_id === "") {
-        return new_id = "aaa";
-    }
-    return new_id;
+    return (new_id === "") ? "aaa" : new_id;
 }
 
 function MakeShort(new_id) { //step 6
-    if(new_id.length >= 16) {
-        new_id = new_id.substr(0,15); //subsrt(startIdx, len)
+    if (new_id.length >= 16) {
+        new_id = new_id.substring(0, 15);
     }
-    return new_id.replace(/[.]$/g,"");
+    return new_id.replace(/[.]$/g, "");
 }
 
 function MakeLong(new_id) { //step 7
-    while(new_id.length < 3) {
-        new_id += new_id[new_id.length-1];
+    while (new_id.length < 3) {
+        new_id += new_id[new_id.length - 1];
     }
     return new_id
 }
